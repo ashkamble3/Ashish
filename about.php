3e92f6299dfb842aa38894715f697b143646faca
ashkamble3 <59990312+ashkamble3@users.noreply.github.com>
2020-01-17 10:31:38 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+<head>
+
+<!-- Meta Tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+<meta name="description" content="Medinova - Health & Medical HTML Template" />
+<meta name="keywords" content="building,business,construction,cleaning,transport,workshop" />
+<meta name="author" content="ThemeMascot" />
+
+<!-- Page Title -->
+<title>Medinova - Health & Medical HTML Template</title>
+
+<!-- Favicon and Touch Icons -->
+<link href="images/favicon.png" rel="shortcut icon" type="image/png">
+<link href="images/apple-touch-icon.png" rel="apple-touch-icon">
+<link href="images/apple-touch-icon-72x72.png" rel="apple-touch-icon" sizes="72x72">
+<link href="images/apple-touch-icon-114x114.png" rel="apple-touch-icon" sizes="114x114">
+<link href="images/apple-touch-icon-144x144.png" rel="apple-touch-icon" sizes="144x144">
+
+<!-- Stylesheet -->
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="css/jquery-ui.min.css" rel="stylesheet" type="text/css">
+<link href="css/animate.css" rel="stylesheet" type="text/css">
+<link href="css/css-plugin-collections.css" rel="stylesheet"/>
+<!-- CSS | menuzord megamenu skins -->
+<link id="menuzord-menu-skins" href="css/menuzord-skins/menuzord-boxed.css" rel="stylesheet"/>
+<!-- CSS | Main style file -->
+<link href="css/style-main.css" rel="stylesheet" type="text/css">
+<!-- CSS | Preloader Styles -->
+<link href="css/preloader.css" rel="stylesheet" type="text/css">
+<!-- CSS | Custom Margin Padding Collection -->
+<link href="css/custom-bootstrap-margin-padding.css" rel="stylesheet" type="text/css">
+<!-- CSS | Responsive media queries -->
+<link href="css/responsive.css" rel="stylesheet" type="text/css">
+<!-- CSS | Style css. This is the file where you can place your own custom css code. Just uncomment it and use it. -->
+<!-- <link href="css/style.css" rel="stylesheet" type="text/css"> -->
+
+<!-- CSS | Theme Color -->
+<link href="css/colors/theme-skin-blue.css" rel="stylesheet" type="text/css">
+<link href="css/ashstyle.css" rel="stylesheet" type="text/css">
+
+<!-- external javascripts -->
+<script src="js/jquery-2.2.0.min.js"></script>
+<script src="js/jquery-ui.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- JS | jquery plugin collection for this theme -->
+<script src="js/jquery-plugin-collection.js"></script>
+
+</head>
+<body class="">
+<div id="wrapper">
+  
+ <?php include 'header.php'; ?>
+  
+  <!-- Start main-content -->
+  <div class="main-content">
+    <!-- Section: inner-header -->
+    <section class="inner-header divider layer-overlay overlay-deep" data-bg-img="myimg/contact.jpg" background-position: center top>
+      <div class="container pt-40 pb-40">
+        <div class="section-content">
+          <div class="row"> 
+            <div class="col-md-12 xs-text-center">
+              <h2 class="font-28">About</h2>
+              <ol class="breadcrumb white mt-10">
+                <li><a href="#">Home</a></li>
+                <li class="active text-theme-colored">Contact</li>
+              </ol>
+            </div>
+          </div>
+        </div>
+      </div>      
+    </section>
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center abc">
+          <h1> Heading</h1>
+        </div>
+        </div>
+        </div>
+    </section>
+
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3 col-xs-12 abc">          
+          <img src="myimg/t2.png">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio dicta, praesentium quibusdam perspiciatis provident fugiat nostrum odio accusamus nam beatae consectetur quisquam error similique cupiditate tempora minus dolorum possimus nulla.</p>
+      </div>
+      <div class="col-md-3 abc">
+          <img src="myimg/t3.png">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio dicta, praesentium quibusdam perspiciatis provident fugiat nostrum odio accusamus nam beatae consectetur quisquam error similique cupiditate tempora minus dolorum possimus nulla.</p>
+        </div>
+        <div class="col-md-3 abc">
+          <img src="myimg/t4.png">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio dicta, praesentium quibusdam perspiciatis provident fugiat nostrum odio accusamus nam beatae consectetur quisquam error similique cupiditate tempora minus dolorum possimus nulla.</p>
+        </div>
+        <div class="col-md-3 abc">
+          <img src="myimg/t5.png">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio dicta, praesentium quibusdam perspiciatis provident fugiat nostrum odio accusamus nam beatae consectetur quisquam error similique cupiditate tempora minus dolorum possimus nulla.</p>
+        </div>
+    </div>
+  </section>
+
+  
+  <!-- end main-content -->
+  
+  <?php include 'footer.php'; ?>
+
+</div>
+
+<script src="js/custom.js"></script>
+
+</body>
+</html>
